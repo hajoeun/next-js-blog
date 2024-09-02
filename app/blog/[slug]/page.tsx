ae41a59ee4b6654fd484890a5617a5aadc8d4667
hajoeun <hajoeun90@gmail.com>
2024-09-02 23:42:58 +0900

    implement view count

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -2,6 +2,7 @@ import { notFound } from "next/navigation";
 import { CustomMDX } from "app/components/mdx";
 import { formatDate, getBlogPosts } from "app/blog/utils";
 import { baseUrl } from "app/sitemap";
+import { sql } from "@vercel/postgres";
 
 export async function generateStaticParams() {
   let posts = getBlogPosts();
@@ -54,20 +55,35 @@ export function generateMetadata({ params }) {
 export async function getViewsCount(): Promise<
   { slug: string; count: number }[]
 > {
-  // if (!process.env.POSTGRES_URL) {
-  //   return [];
-  // }
+  "use server";
+  if (!process.env.POSTGRES_URL) {
+    return [];
+  }
 
-  return [{ slug: "vim", count: 1234 }];
+  const { rows } = await sql`
+    SELECT slug, count
+    FROM views
+  `;
 
-  // return sql`
-  //   SELECT slug, count
-  //   FROM views
-  // `;
+  return rows.map((row) => ({
+    slug: row.slug,
+    count: row.count,
+  }));
 }
+
+const incrementView = async (slug: string) => {
+  "use server";
+
+  await sql`
+    INSERT INTO views (slug, count)
+    VALUES (${slug}, 1)
+    ON CONFLICT (slug) DO UPDATE SET count = views.count + 1 
+  `;
+};
 
 export default async function Blog({ params }) {
   let post = getBlogPosts().find((post) => post.slug === params.slug);
+  await incrementView(params.slug);
   const views = await getViewsCount();
   const count = views.find((view) => view.slug === params.slug)?.count || 0;
 
